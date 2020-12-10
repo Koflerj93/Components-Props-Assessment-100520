6e8e83ba9a2341614c2783b9529b323d1f2c03e8
Koflerj93 <koflerj93@gmail.com>
2020-12-10 08:45:06 -0500

    done

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 
+
 function Note(props) {
     return <li>{props.note.content}</li>
 }
--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -6,10 +6,15 @@ class NotesContainer extends React.Component {
         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
     }
 
+    apiArray = () => { 
+        // console.log(this.apiResponse())
+       return this.apiResponse().map(noteEl => <Note note = {noteEl} key = {noteEl.id} />)
+    }
+
     render() {
         return (
             <ul>
-                {}
+                {this.apiArray()}
             </ul>
         )
     }
